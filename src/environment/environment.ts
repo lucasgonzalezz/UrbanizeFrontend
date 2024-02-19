682ad8c8e75fcdbe2787343179cb841f675508ba
ElPanita04 <lucgr04.vlc@gmail.com>
2024-02-19 19:21:13 +0100

    version normal

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from "@angular/common/http";
 
-export const API_URL: string = 'http://pro.ausiasmarch.es:50548';
+export const API_URL: string = 'http://localhost:8081';
 
 export const httpOptions = {
     headers: new HttpHeaders({
